62b7afedd195c5694e4fd3b7c6eb123f1de981eb
Ivan Reis <114751660+ivanreis96@users.noreply.github.com>
2026-07-19 11:34:57 -0300

    Exclusão do diretório dist
    Deletando a pasta dist, foi para o repositório equivocadamente.
    Corrigido pelo gitignore

--- dist/user.d.ts
+++
@@ -1,8 +0,0 @@
-import { z } from 'zod';
-export declare const userProfileSchema: z.ZodObject<{
-    id: z.ZodString;
-    displayName: z.ZodString;
-    email: z.ZodString;
-    avatarUrl: z.ZodOptional<z.ZodString>;
-}, z.core.$strip>;
-export type UserProfile = z.infer<typeof userProfileSchema>;
