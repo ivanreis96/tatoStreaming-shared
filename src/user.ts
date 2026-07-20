362f3e16d66917d07ac460b16fd6c42571c4c994
Ivan Reis <ivreis96@gmail.com>
2026-07-20 17:14:41 -0300

    Corrigindo código deprecated do zod na autentificação

--- src/user.ts
+++ src/user.ts
@@ -3,8 +3,8 @@ import { z } from 'zod';
 export const userProfileSchema = z.object({
   id: z.string().min(1, 'Id do usuário é obrigatório.'),
   displayName: z.string().min(2, 'Nome de exibição deve ter no mínimo 2 caracteres.'),
-  email: z.string().email('E-mail inválido.'),
-  avatarUrl: z.string().url('Avatar inválido.').optional(),
+  email: z.email('E-mail inválido.'),
+  avatarUrl: z.url('Avatar inválido.').optional(),
 });
 
 export type UserProfile = z.infer<typeof userProfileSchema>;
